263ba7b931b10c722cc5cc3fb0e9956479fbe948
shalom bergman <shalom195295@gmail.com>
2024-11-14 14:48:39 +0200

    create a blueprints email api

---
+++ blueprints/email_bp.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, request, jsonify
+
+from emails_producer.emails_producer import all_email
+from main_service.database import save_to_mongo
+
+email_bp = Blueprint('email_bp', __name__)
+@email_bp.route('api/email', methods=['POST'])
+def email():
+    data = request.get_json()
+    if not data:
+        return jsonify({'error': 'no data'}), 400
+    all_email(data)
+    save_to_mongo(data)
+
+    return jsonify(data), 201
+

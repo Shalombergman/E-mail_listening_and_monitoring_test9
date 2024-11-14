3a79d5e9fcb83dd06cd455f428acbf869b06ac5e
shalom bergman <shalom195295@gmail.com>
2024-11-14 11:41:01 +0200

    chance a type of "device_id" to String

--- main_service/models/device_info_model.py
+++ main_service/models/device_info_model.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from sqlalchemy import Column, Integer, String, ForeignKey, Index
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship

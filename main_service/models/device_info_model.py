fa26b15ed5c15e803a09efde5917cb58fded8966
shalom bergman <shalom195295@gmail.com>
2024-11-14 12:58:25 +0200

    create a User model and chance the models and raealationship

--- main_service/models/device_info_model.py
+++ main_service/models/device_info_model.py
@@ -1,10 +1,11 @@
 from uuid import uuid4
 
 from sqlalchemy import Column, Integer, String, ForeignKey, Index
-from sqlalchemy.ext.declarative import declarative_base
+
 from sqlalchemy.orm import relationship
 
-Base = declarative_base()
+from main_service.models.Base import Base
+
 
 class DeviceInfoModel(Base):
     __tablename__ = 'device_info'
@@ -12,3 +13,4 @@ class DeviceInfoModel(Base):
     browser = Column(String)
     os = Column(String)
     device_id = Column(String)
+    user = relationship('UserModel', back_populates='device_info', uselist=False)
